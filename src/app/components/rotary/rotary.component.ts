e2dd66f2de6084cda5a7b247b74069f221a05a53
Kyle Kemp <kyle@seiyria.com>
2023-12-13 16:07:35 -0600

    make rotary look nice and work

--- src/app/components/rotary/rotary.component.ts
+++ src/app/components/rotary/rotary.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
+import { SuikaFruit } from '../../../interfaces';
 
 @Component({
   selector: 'app-rotary',
   templateUrl: './rotary.component.html',
-  styleUrl: './rotary.component.scss'
+  styleUrl: './rotary.component.scss',
 })
 export class RotaryComponent {
-
+  public readonly fruits = [
+    [
+      {
+        fruit: SuikaFruit.Cherry,
+        size: 24,
+      },
+      {
+        fruit: SuikaFruit.Strawberry,
+        size: 32,
+      },
+    ],
+    [
+      {
+        fruit: SuikaFruit.Grape,
+        size: 40,
+      },
+      {
+        fruit: SuikaFruit.Clementine,
+        size: 48,
+      },
+    ],
+    [
+      {
+        fruit: SuikaFruit.Orange,
+        size: 56,
+      },
+      {
+        fruit: SuikaFruit.Pomegranate,
+        size: 64,
+      },
+    ],
+    [
+      {
+        fruit: SuikaFruit.Lemon,
+        size: 72,
+      },
+      {
+        fruit: SuikaFruit.Peach,
+        size: 80,
+      },
+    ],
+    [
+      {
+        fruit: SuikaFruit.Pineapple,
+        size: 88,
+      },
+      {
+        fruit: SuikaFruit.Cantaloupe,
+        size: 96,
+      },
+    ],
+    [
+      {
+        fruit: SuikaFruit.Watermelon,
+        size: 208,
+      },
+    ],
+  ];
 }
